c7cd43c98a2a7beb67e6c48b777c65d206623dd5
Firdavs Khaydarov <recouse@mail.ru>
2018-06-30 13:30:24 +0500

    Initial Commit

---
+++ ImageCarousel/ImageCarousel.h
@@ -0,0 +1,19 @@
+//
+//  ImageCarousel.h
+//  ImageCarousel
+//
+//  Created by Firdavs Khaydarov on 30/06/18.
+//  Copyright © 2018 Firdavs Khaydarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageCarousel.
+FOUNDATION_EXPORT double ImageCarouselVersionNumber;
+
+//! Project version string for ImageCarousel.
+FOUNDATION_EXPORT const unsigned char ImageCarouselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageCarousel/PublicHeader.h>
+
+
